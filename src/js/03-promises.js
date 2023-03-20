829f677f76f1142c4be336c21f1eeea29bba779a
SlavaKorGo <114766759+SlavaKorGo@users.noreply.github.com>
2023-03-20 20:07:05 +0200

    fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,48 +1,48 @@
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
 
-const form = document.querySelector('.form')
+const form = document.querySelector('.form');
 
-form.addEventListener('submit', onSubmitForm)
-
+form.addEventListener('submit', onSubmitForm);
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  return new Promise ((resolve, reject) =>{
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
         // Fulfill
-        resolve ({position, delay});
+        resolve({ position, delay });
       } else {
         // Reject
-        reject ({position, delay});
+        reject({ position, delay });
       }
     }, delay);
   });
 }
 
-function onSubmitForm (e) {
+function onSubmitForm(e) {
   e.preventDefault();
 
   const {
-    elements: {delay: delayRef, step: stepRef, amount: amountRef},
-  } = form; 
+    elements: { delay: delayRef, step: stepRef, amount: amountRef },
+  } = form;
 
   let position = 1;
-  let delay = Number(delayRef.velue);
-  const amount = Number(amountRef.velue);
+  let delay = Number(delayRef.value);
+  const amount = Number(amountRef.value);
   while (position <= amount) {
-    createPromise(position, delay).then(({ position, delay }) => {
-      Notiflix.Notify.success
-      (`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      Notiflix.Notify.success
-      (`❌ Rejected promise ${position} in ${delay}ms`);
-    });
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
     delay += Number(stepRef.value);
-    position +=1;
+    position += 1;
   }
-  
 }
-
