bdc5446f72fe00a60548a4aeca5c1160b78d5d6e
SlavaKorGo <114766759+SlavaKorGo@users.noreply.github.com>
2023-03-21 20:26:02 +0000

    Deploying to gh-pages from @ SlavaKorGo/goit-js-hw-09@718547073b6e01f29445a3e4cc5c1a35f159f494 🚀

---
+++ 01-color-switcher.1c1761cb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),a=null;e.setAttribute("disabled","");var d=function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))};t.addEventListener("click",(function(){a=setInterval(d,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.1c1761cb.js.map
